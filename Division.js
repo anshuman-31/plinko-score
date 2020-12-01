f8e9c31d9572366ae26d51bf962c62fd0d9f88b1
anshuman-31 <70523558+anshuman-31@users.noreply.github.com>
2020-12-01 08:27:31 +0530

    Add files via upload

---
+++ Division.js
@@ -0,0 +1,18 @@
+class Division {
+    constructor(x,y,width,height){
+        var options={
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+       // World.add(World,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+      //  rectMode(CENTRE);
+        fill("white");
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+
+};
